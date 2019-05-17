a896bc6b18854d3b4f89ebba367cad9fcd090112
sfcline <sfcline1608@eagle.fgcu.edu>
2019-05-16 22:24:04 -0400

    The menu has been implemented.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int option;
-    cout << "Please enter a number 1~5 for the following options.\n"
+    cout << "Please enter a number 1~5 for the following options.\n\n"
             "1. Make an employee account\n"
             "2. Make a Music Player\n"
             "3. Make a Movie Player\n"
