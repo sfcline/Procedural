b1ae5b2a35b111176a3723f82fadde2dd92ef7ac
sfcline <sfcline1608@eagle.fgcu.edu>
2019-05-16 22:19:02 -0400

    The menu has been implemented.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int option;
+    cout << "Please enter a number 1~5 for the following options.\n"
+            "1. Make an employee account\n"
+            "2. Make a Music Player\n"
+            "3. Make a Movie Player\n"
+            "4. Add a new product\n"
+            "5. Display collection\n" << endl;
+    cin >> option;
+    if (0 < option && option < 6 )
+        cout << "Hello World!" << endl;
+    else
+        cout << "A number 1 through five was not entered." << endl;
     return 0;
 }
