547ebf631cc4a27b24ea8ef1f8b2830f74150375
sfcline <sfcline1608@eagle.fgcu.edu>
2019-06-20 14:52:45 -0400

    Bug fixes and overall quality updates.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 /// @file main.cpp
 /// @brief This program displays the products that are produced and the amount of each for a media player production facility.
 /// @author Stephen Cline
-/// @bug Produce items still asks for number of items produced when there are no products to select
-/// @bug If a product is typed in with a space it creates an infinite loop
 
 #include <iostream>
 #include <iomanip>
@@ -35,25 +33,25 @@ int selectMenu(vector<Product> &, vector<Record> &, vector<Statistic> &);
 
 void newProduct(vector<Product> &);
 
-void production(const string &, const string &, vector<Record> &);
+void production(const int &, const string &, const vector<Product> &, vector<Record> &);
 
-void productionView(const string &);
+void productionView(const string &, const vector<Record> &);
 
-string catalogMenu(string &, const vector<Product> &);
+int catalogMenu(string &, const vector<Product> &);
 
 void serialSearch();
 
 void accountCreation();
 
-string encrypt_string(string);
+string encryptString(string);
 
-string decrypt_string(string);
+string decryptString(string);
 
-Product make_product(string, string, string);
+Product makeProduct(const string &, const string &, const string &);
 
-Record make_record(int, string);
+Record makeRecord(const int &, const string &);
 
-Statistic make_statistic(int, string);
+Statistic makeStatistic(const int &, const string &);
 
 void loadProductLine(vector<Product> &);
 
@@ -62,16 +60,16 @@ void loadProductRecords(vector<Record> &);
 void loadProductStats(vector<Statistic> &, const vector<Record> &);
 
 int main() {
-    vector<Product> product_line;
-    vector<Record> product_records;
-    vector<Statistic> product_stats;
+    vector<Product> productLine;
+    vector<Record> productRecords;
+    vector<Statistic> productStats;
     int option;
-    loadProductLine(product_line);
-    loadProductRecords(product_records);
-    loadProductStats(product_stats, product_records);
+    loadProductLine(productLine);
+    loadProductRecords(productRecords);
+    loadProductStats(productStats, productRecords);
     cout << "Production Line Tracker\n\n";
     do {
-        option = selectMenu(product_line, product_records, product_stats);
+        option = selectMenu(productLine, productRecords, productStats);
     } while (option != 6);
 
     return 0;
@@ -79,9 +77,9 @@ int main() {
 
 /// @brief This function allows the user to select from the main menu.
 /// @param option
-int selectMenu(vector<Product> &product_line, vector<Record> &product_records, vector<Statistic> &product_stats) {
+int selectMenu(vector<Product> &productLine, vector<Record> &productRecords, vector<Statistic> &productStats) {
     string itemTypeCode;
-    string catalogOption;
+    int catalogOption;
     int option;
     cout << "1. Produce Items\n"
             "2. Add Employee Account\n"
@@ -92,20 +90,21 @@ int selectMenu(vector<Product> &product_line, vector<Record> &product_records, v
     cin >> option;
     switch (option) {
         case 1:
-            catalogOption = catalogMenu(itemTypeCode, product_line);
-            production(catalogOption, itemTypeCode, product_records);
+            catalogOption = catalogMenu(itemTypeCode, productLine);
+            production(catalogOption, itemTypeCode, productLine, productRecords);
             break;
         case 2:
             accountCreation();
             break;
         case 3:
             cout << "\n";
-            newProduct(product_line);
+            newProduct(productLine);
             break;
         case 4:
             cout << "\n";
-            catalogOption = catalogMenu(itemTypeCode, product_line);
-            productionView(catalogOption);
+            catalogOption = catalogMenu(itemTypeCode, productLine);
+            if(catalogOption > -1)
+                productionView(productLine[catalogOption].manufacturer, productRecords);
             break;
         case 5:
             serialSearch();
@@ -119,7 +118,7 @@ int selectMenu(vector<Product> &product_line, vector<Record> &product_records, v
 }
 
 /// @brief This function allows the user to enter a new product into the catalog.
-void newProduct(vector<Product> &product_line) {
+void newProduct(vector<Product> &productLine) {
     cout << "Enter the Manufacturer\n";
     string manufacturer;
     cin >> manufacturer;
@@ -156,12 +155,15 @@ void newProduct(vector<Product> &product_line) {
     catalog.open("ProductLine.csv");
     catalog << manufacturer << "," << prodName << "," << itemTypeCode << "\n";
     catalog.close();
-    product_line.push_back(make_product(manufacturer, prodName, itemTypeCode));
+    productLine.push_back(makeProduct(manufacturer, prodName, itemTypeCode));
 }
 
 /// @brief This function allows the user to add new production logs.
 /// @param manufacturer
-void production(const string &manufacturer, const string &itemTypeCode, vector<Record> &product_record) {
+void production(const int &catalogOption, const string &itemTypeCode, const vector<Product> &productLine, vector<Record> &productRecord) {
+    if (catalogOption == -1)
+        return;
+    string manufacturer = productLine[catalogOption].manufacturer;
     int numProduced = 1;
     cout << "Enter the number of items that were produced\n";
     cin >> numProduced;
@@ -170,74 +172,69 @@ void production(const string &manufacturer, const string &itemTypeCode, vector<R
     prodLog.open("ProductionLog.csv");
     string tmpProdLog;
 
-    int i = 1;
-    for (const auto &x : product_record) {
+    int count = 1;
+    for (const auto &x : productRecord) {
         if (string::npos != x.serial.find(manufacturer.substr(0, 3)))
-            i++;
+            count++;
     }
-    numProduced = i + numProduced;
-    for (i; i <= numProduced; i++) {
+    numProduced = count + numProduced;
+    for (int i = count; i < numProduced; i++) {
         if (i < 10)
             tmpProdLog = to_string(i) + "," + manufacturer.substr(0, 3) + itemTypeCode + "0000" + to_string(i);
-        else if (10 < i && i < 100)
+        else if (10 <= i && i < 100)
             tmpProdLog = to_string(i) + "," + manufacturer.substr(0, 3) + itemTypeCode + "000" + to_string(i);
-        else if (100 < i && i < 1000)
+        else if (100 <= i && i < 1000)
             tmpProdLog = to_string(i) + "," + manufacturer.substr(0, 3) + itemTypeCode + "00" + to_string(i);
-        else if (1000 < i && i < 10000)
+        else if (1000 <= i && i < 10000)
             tmpProdLog = to_string(i) + "," + manufacturer.substr(0, 3) + itemTypeCode + "0" + to_string(i);
         else
             tmpProdLog = to_string(i) + "," + manufacturer.substr(0, 3) + itemTypeCode + to_string(i);
         prodLog << tmpProdLog << "\n";
-        product_record.push_back(make_record(i, tmpProdLog));
+        productRecord.push_back(makeRecord(i, tmpProdLog.substr(tmpProdLog.find(manufacturer.substr(0, 3)))));
     }
     prodLog.close();
 }
 
 /// @brief This function displays production logs according to the user's choice.
 /// @param catalogOption
-void productionView(const string &catalogOption) {
-    string line;
-    ifstream prodLog;
-    prodLog.open("ProductionLog.csv");
-    while (getline(prodLog, line)) {
-        if (string::npos != line.find(catalogOption.substr(0, 3))) {
-            cout << line << "\n";
-        }
+/// @param productRecords
+void productionView(const string &catalogOption, const vector<Record> &productRecords) {
+    for (const auto &x : productRecords) {
+        if (string::npos != x.serial.find(catalogOption.substr(0, 3)))
+            cout << x.orderNumber << ". " << x.serial << "\n";
     }
 }
 
 /// @brief This function displays the catalog menu.
+/// @param itemTypeCode
+/// @param productLine
 /// @return catalog option from user input
-string catalogMenu(string &itemTypeCode, const vector<Product> &product_line) {
-    bool catalogMatch = false;
-    string catalogOption;
+int catalogMenu(string &itemTypeCode, const vector<Product> &productLine) {
+    int catalogOption = 0;
+    int count = 0;
     ifstream catalog;
     catalog.open("ProductLine.csv");
     if (!catalog.good()) {
+        catalog.close();
         cout << "No available products\n";
-        return "00000";
+        return -1;
     }
     catalog.close();
 
-    cout << "Select a Product\nDo not use spaces\n";
+    cout << "Select a Product\n";
 
-    while (!catalogMatch) {
-        for (const auto &x : product_line)
-            cout << x.manufacturer << " " << x.name << "\n";
+    while (catalogOption < 1 || catalogOption > count) {
+        for (const auto &x : productLine)
+            cout << ++count << ". " << x.manufacturer << " " << x.name << "\n";
         cin >> catalogOption;
-        for (auto &i : product_line) {
-            if (string::npos != i.manufacturer.find(catalogOption.substr(0, 3)) ||
-                string::npos != i.name.find(catalogOption.substr(0, 3))) {
-                catalogMatch = true;
-                catalogOption = i.manufacturer;
-                itemTypeCode = i.type;
-            }
+        if (0 < catalogOption <= count) {
+            itemTypeCode = productLine[catalogOption - 1].type;
         }
-        if (!catalogMatch)
+        else
             cout << "Not a valid selection\n";
     }
 
-    return catalogOption;
+    return catalogOption - 1;
 }
 
 /// @brief This function searches for a product by serial.
@@ -269,17 +266,17 @@ void accountCreation() {
     /// Create Username
     cout << "Enter employee's full name\n";
 
-    string first_name;
-    cin >> first_name;
-    string last_name;
-    cin >> last_name;
+    string firstName;
+    cin >> firstName;
+    string lastName;
+    cin >> lastName;
 
-    string user_name;
+    string userName;
 
-    // create user name in proper format
-    last_name[0] = tolower(last_name[0]);
-    last_name.insert(last_name.begin(), tolower(first_name[0]));
-    user_name = last_name;
+    /// create user name in proper format
+    lastName[0] = tolower(lastName[0]);
+    lastName.insert(lastName.begin(), tolower(firstName[0]));
+    userName = lastName;
 
     /// Create password
     cout << "Enter employee's password.\n";
@@ -293,7 +290,7 @@ void accountCreation() {
             && any_of(password.begin(), password.end(), ::isupper)
             && all_of(password.begin(), password.end(), ::isalnum)) {
             valid = true;
-            cout << "User name: " + user_name + "\n"
+            cout << "User name: " + userName + "\n"
                  << "Password: Valid\n";
 
         } else
@@ -301,27 +298,27 @@ void accountCreation() {
     }
     ofstream accounts;
     accounts.open("Users.txt");
-    accounts << user_name << "," << encrypt_string(password) << "\n";
+    accounts << userName << "," << encryptString(password) << "\n";
     accounts.close();
 }
 
 /// @brief This function encrypts a string with recursion
 /// @param str
-string encrypt_string(string str) {
+string encryptString(string str) {
     if (str.length() == 1) {
         return str;
     } else {
-        return char((int) str[0] + 3) + encrypt_string(str.substr(1, str.length() - 1));
+        return char((int) str[0] + 3) + encryptString(str.substr(1, str.length() - 1));
     }
 }
 
 /// @brief This function decrypts a string with recursion
 /// @param str
-string decrypt_string(string str) {
+string decryptString(string str) {
     if (str.length() == 1) {
         return str;
     } else {
-        return char((int) str[0] - 3) + decrypt_string(str.substr(1, str.length() - 1));
+        return char((int) str[0] - 3) + decryptString(str.substr(1, str.length() - 1));
     }
 }
 
@@ -329,30 +326,30 @@ string decrypt_string(string str) {
 /// @param manufacturer
 /// @param name
 /// @param type
-Product make_product(string manufacturer, string name, string type) {
-    Product product_line = {manufacturer, name, type};
-    return product_line;
+Product makeProduct(const string &manufacturer, const string &name, const string &type) {
+    Product productLine = {manufacturer, name, type};
+    return productLine;
 }
 
 /// @brief This function creates and returns a new record in the struct format
 /// @param total
 /// @param serial
-Record make_record(int total, string serial) {
-    Record product_record = {total, serial};
-    return product_record;
+Record makeRecord(const int &total, const string &serial) {
+    Record productRecord = {total, serial};
+    return productRecord;
 }
 
 /// @brief This function creates and returns a new statistic in the struct format
 /// @param total
 /// @param type
-Statistic make_statistic(int total, string type) {
-    Statistic product_stat = {total, type};
-    return product_stat;
+Statistic makeStatistic(const int &total, const string &type) {
+    Statistic productStat = {total, type};
+    return productStat;
 }
 
 /// @brief This function loads the production line
-/// @param product_line
-void loadProductLine(vector<Product> &product_line) {
+/// @param productLine
+void loadProductLine(vector<Product> &productLine) {
     ifstream catalog;
     catalog.open("ProductLine.csv");
     if (!catalog.good()) {
@@ -370,14 +367,14 @@ void loadProductLine(vector<Product> &product_line) {
         getline(tmpStream, manufacturer, ',');
         getline(tmpStream, name, ',');
         getline(tmpStream, type, ',');
-        product_line.push_back(make_product(manufacturer, name, type));
+        productLine.push_back(makeProduct(manufacturer, name, type));
     }
     catalog.close();
 }
 
 /// @brief This function loads the product records
-/// @param product_records
-void loadProductRecords(vector<Record> &product_records) {
+/// @param productRecords
+void loadProductRecords(vector<Record> &productRecords) {
     string total;
     string serial;
     string line;
@@ -387,21 +384,21 @@ void loadProductRecords(vector<Record> &product_records) {
         stringstream tmpStream(line);
         getline(tmpStream, total, ',');
         getline(tmpStream, serial, ',');
-        product_records.push_back(make_record(stoi(total), serial));
+        productRecords.push_back(makeRecord(stoi(total), serial));
     }
     prodLog.close();
 }
 
 /// @brief This function loads the product stats of total items produced by type
-/// @param product_stats
-void loadProductStats(vector<Statistic> &product_stats, const vector<Record> &product_records) {
+/// @param productStats
+void loadProductStats(vector<Statistic> &productStats, const vector<Record> &productRecords) {
     int All = 0;
     int MM = 0;
     int VI = 0;
     int AM = 0;
     int VM = 0;
 
-    for (const auto &x : product_records) {
+    for (const auto &x : productRecords) {
         if (string::npos != x.serial.find("MM"))
             MM++;
         if (string::npos != x.serial.find("VI"))
@@ -413,9 +410,9 @@ void loadProductStats(vector<Statistic> &product_stats, const vector<Record> &pr
         All++;
     }
 
-    product_stats.push_back(make_statistic(All, "All"));
-    product_stats.push_back(make_statistic(MM, "MM"));
-    product_stats.push_back(make_statistic(VI, "VI"));
-    product_stats.push_back(make_statistic(AM, "AM"));
-    product_stats.push_back(make_statistic(VM, "VM"));
+    productStats.push_back(makeStatistic(All, "All"));
+    productStats.push_back(makeStatistic(MM, "MM"));
+    productStats.push_back(makeStatistic(VI, "VI"));
+    productStats.push_back(makeStatistic(AM, "AM"));
+    productStats.push_back(makeStatistic(VM, "VM"));
 }
